0a02489386138edc5133437aab9441bf2976e788
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-08-30 18:32:54 -0300

    feat: add authenticate controller

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,30 +1,50 @@
-import { ConflictException, UsePipes } from '@nestjs/common';
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { hash } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UnauthorizedException, UsePipes } from '@nestjs/common';
+
 import { z } from 'zod';
+import { compare } from 'bcryptjs';
 
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
-import { JwtService } from '@nestjs/jwt';
 
-const createAccountBodySchema = z.object({
-  name: z.string(),
+const authenticateBodySchema = z.object({
   email: z.string().email(),
   password: z.string(),
 });
 
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   @Post()
-  //   @HttpCode(201)
-  //   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-  async handle(@Body() body: CreateAccountBodySchema) {
-    const token = this.jwt.sign({ sub: 'user-id' });
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
 
-    return token;
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User credentials do not match');
+    }
+
+    const isValidPassword = await compare(password, user.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('User credentials do not match');
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id });
+
+    return { access_token: accessToken };
   }
 }
