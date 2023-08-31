91e5e6395dd0881b08375f1d2fee070f08fa2c9c
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-08-31 08:24:30 -0300

    feat: add create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -9,7 +9,10 @@ import { UserPayload } from 'src/auth/jwt.strategy';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 
-const createQuestionBodySchema = z.object({});
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
 
 type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
@@ -19,7 +22,33 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@CurrentUser() user: UserPayload) {
-    return 'ok';
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body;
+
+    const userId = user.sub;
+
+    const slug = this.convertToSlug(title);
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-');
   }
 }
