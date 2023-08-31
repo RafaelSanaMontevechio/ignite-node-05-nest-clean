b933c14c03e13592251b706e606cdea6d8bf71e7
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-08-31 08:59:35 -0300

    feat: add testing configs

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -3,11 +3,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { z } from 'zod';
-import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
+import { ZodValidationPipe } from '@/pipes/zod-validation.pipe';
 
-import { UserPayload } from 'src/auth/jwt.strategy';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { CurrentUser } from 'src/auth/current-user.decorator';
+import { UserPayload } from '@/auth/jwt.strategy';
+import { PrismaService } from '@/prisma/prisma.service';
+import { CurrentUser } from '@/auth/current-user.decorator';
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { z } from 'zod';
-import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
+import { ZodValidationPipe } from '@/pipes/zod-validation.pipe';
 
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '@/prisma/prisma.service';
 
 const pageQueryParamSchema = z
   .string()
