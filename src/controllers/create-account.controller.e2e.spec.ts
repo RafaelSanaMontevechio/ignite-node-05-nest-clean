4a470af5fb88da8826d7ae7535cdb326a93a0285
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-01 09:07:25 -0300

    test: add create account e2e test

--- src/controllers/create-account.controller.e2e.spec.ts
+++ src/controllers/create-account.controller.e2e.spec.ts
@@ -1,3 +1,30 @@
-test('1 plus 1', () => {
-  console.log('1 plus 1');
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+
+import request from 'supertest';
+
+import { AppModule } from '@/app.module';
+
+describe('Create account (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    await app.init();
+  });
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+  });
 });
