2fd0607379290151e1bc261a85ab88efd0f44631
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-07 15:21:27 -0300

    feat: add student usee cases

---
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+
+import { Either, left, right } from '@/core/either';
+
+import { Student } from '../../enterprise/entities/student';
+
+import { StudentsRepository } from '../repositories/students-repository';
+import { HashGenerator } from '../cryptography/hash-generator';
+
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error';
+
+interface RegisterStudentUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type RegisterStudentUseCaseResponse = Either<
+  StudentAlreadyExistsError,
+  {
+    student: Student;
+  }
+>;
+
+@Injectable()
+export class RegisterStudentUseCase {
+  constructor(
+    private studentRepository: StudentsRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterStudentUseCaseRequest): Promise<RegisterStudentUseCaseResponse> {
+    const studentWithTheSameEmail =
+      await this.studentRepository.findByEmail(email);
+
+    if (studentWithTheSameEmail) {
+      return left(new StudentAlreadyExistsError(email));
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password);
+
+    const student = Student.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await this.studentRepository.create(student);
+
+    return right({
+      student,
+    });
+  }
+}
