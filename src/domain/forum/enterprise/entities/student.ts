40c3d2a8e789a53751956c0edaadf4620875745b
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-07 14:57:05 -0300

    feat: add cryptography gateways

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -1,14 +1,28 @@
-import { Entity } from '@/core/entities/entity'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 interface StudentProps {
-  name: string
+  name: string;
+  email: string;
+  password: string;
 }
 
 export class Student extends Entity<StudentProps> {
-  static create(props: StudentProps, id?: UniqueEntityID) {
-    const student = new Student(props, id)
+  get name() {
+    return this.name;
+  }
 
-    return student
+  get email() {
+    return this.email;
+  }
+
+  get password() {
+    return this.password;
+  }
+
+  static create(props: StudentProps, id?: UniqueEntityID) {
+    const student = new Student(props, id);
+
+    return student;
   }
 }
